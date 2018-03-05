bd8dd9f7a5edc4fed55fcb30ee870427255511ef
Terence Tachiona <tachiona0607@gmail.com>
2018-03-05 09:55:04 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Evaluate-A-Job
-Using the job description data to predict the grade/salary that the organisation should give using convolution neural networks model.
+# Position-Salary-Forecast
+CNN Model : Using the job description data to predict the salary that the company should give.
+
+1.参考论文《Convolutional Neural Networks for Sentence Classification》
+论文链接：http://cslt.riit.tsinghua.edu.cn/mediawiki/images/f/fe/Convolutional_Neural_Networks_for_Sentence_Classi%EF%AC%81cation.pdf
+文中介绍了CNN在文本分类中的应用。考虑在职位薪资预测中使用其中的CNN-rand模型处理文本语义抽象。
+
+2.将职位名称和职位描述信息当作输入文本，增加技能词关键词进行分词并去除纯数字和标点符号的结果，将文中的CNN分类模型改成回归预测模型。
+参考代码：https://github.com/dennybritz/cnn-text-classification-tf
+
+3.随机抽选某城市的7500条职位进行测试，测试结果表明是可以work的，不过由于数据量比较少，造成过拟合严重，正在修改模型和参数，希望下一次结果能够有大的提升
+薪资1代表1000，4500次迭代后的均方误差为训练集：3.726测试集：41.92
+
+4.由于数据从公司数据库中取出的，这里就不上传数据了，如果需要可私信联系我
+
---
+++ data_helpers.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+# @Author:lthan
+# @Email :xiaohan809@qq.com
+# @Time  :2017/9/6 17:18
+
+import numpy as np
+import re
+import jieba
+import jieba.analyse
+from string import punctuation
+import os
+
+class DataHelper(object):
+    def __init__(self):
+        self.my_puctuation = '~·！@#￥%……&*（）｛｝【】、“”；：‘’，。、《》？' + punctuation
+    def load_datasets(self,file_name,keywords_file):
+        '''
+        Load the jobdesc information
+        :param file_name:
+        :param keywords_file:
+        :return:
+        '''
+        x = []
+        y = []
+        jieba.load_userdict(keywords_file)
+        with open(file_name,'r',encoding='utf-8') as fr:
+            for line in fr:
+                # read line
+                try:
+                    line = line.strip().split('\001')
+                    city = line[56].strip()
+                    jobtype = int(line[31])  # 雇佣类型： 全职 兼职 学生职位
+                    if city != '530' or jobtype != 2:
+                        continue
+                    input_x = line[24] # 职位名称
+                    input_x += line[46] # 职位描述
+                    input_x = re.sub(pattern='\d\S',repl='',string=input_x)
+                    word_list = [word.lower() for word in list(jieba.cut(input_x)) if word not in self.my_puctuation]
+                    input_x = " ".join(word_list)
+                    input_y = [(float(line[15]) // 1000 + float(line[16]) // 1000) * 0.5]
+                except ValueError as err:
+                    print('except:{}'.format(err))
+                x.append(input_x)
+                y.append(input_y)
+
+                #print(input_x, input_y)
+        return [x,y]
+
+
+    def batch_iter(self,data,batch_size,num_epochs,shuffle=True):
+        '''
+        return split batch of dataSet
+        :param data:
+        :param batch_size:
+        :param num_epochs:
+        :param shuffle:
+        :return:
+        '''
+        data = np.array(data)
+        data_size = len(data)
+        num_batches_per_epoch = int((len(data) - 1) / batch_size) + 1
+        for epoch in range(num_epochs):
+            # Shuffle the data at each epoch
+            if shuffle:
+                shuffle_indices = np.random.permutation(np.arange(data_size))
+                shuffled_data = data[shuffle_indices]
+            else:
+                shuffled_data = data
+            for batch_num in range(num_batches_per_epoch):
+                start_index = batch_num * batch_size
+                end_index = min((batch_num+1)*batch_size,data_size)
+                yield shuffled_data[start_index:end_index]
